a93dd33ba4c80612d06c7d7ad6d647bc67015935
relavrin <rebeka.lavrin@gmail.com>
2024-01-05 17:38:44 +0100

    Finish 54728

--- Day1_2/Day1_2.cs
+++ Day1_2/Day1_2.cs
@@ -32,9 +32,12 @@ namespace Day1_2
                 }
 
 
-                foreach (string s in lines)
+            
+            foreach (string s in lines)
                 {
-                    for (int i = 0; i < s.Length; i++)
+                firstNum = "";
+                secondNum = "";
+                  /*  for (int i = 0; i < s.Length; i++)
                     {
                         char ch = s[i];
 
@@ -46,33 +49,155 @@ namespace Day1_2
                         {
                             secondNum = ch.ToString();
                         }
-                    }
+                    }*/
 
                     for (int i = 0; i < s.Length; i++)
                     {
-                        char ch = s[i];
-                        for (int j = i; j < i + 2; j++)
+                        
+                        for (int j = i; j < i + 3 && j <= s.Length-1; j++)
                         {
-                            check += check.ToString();
+                            char ch = s[j];
+                            if(char.IsDigit(ch)) 
+                            {
+                                firstNum = ch.ToString();
+                                break;
+                            }
+                            check += ch.ToString();
                         }
 
-                        if (firstNum == "" && check == "one" || check == "two" || check == "thr" || check == "fou" || check == "fiv" || check == "six" || check == "sev" || check == "eig" || check == "nin")
+                        
+                        if(check == "one" || check == "two" || check == "thr" || check == "fou" || check == "fiv" || check == "six" || check == "sev" || check == "eig" || check == "nin")
                         {
-                            firstNum = check;
+                                firstNum = check;
+                                break;
+                        
                         }
-                        else if (secondNum == "" && check == "one" || check == "two" || check == "thr" || check == "fou" || check == "fiv" || check == "six" || check == "sev" || check == "eig" || check == "nin")
+                        if (!string.IsNullOrEmpty(firstNum))
                         {
-                            secondNum = check;
+                            break;
                         }
+                        
+                        check = "";
+                    }
+                    check = "";
+                    string flipped = "";
+                for (int i = s.Length - 1; i > 0; i--)
+                {
+                    for (int j = i; j > i - 3 && j >= 0; j--)
+                    {
+                        char ch = s[j];
+                        if (char.IsDigit(ch))
+                        {
+                            secondNum = ch.ToString();
+                            break;
+                        }
+                        check += ch.ToString();
                     }
 
-                    sum = firstNum + secondNum;
-                    numbers.Add(sum);
+                    flipped = Flip(check);
+                   
+                        if (flipped == "one" || flipped == "two" || flipped == "thr" || flipped == "fou" || flipped == "fiv" || flipped == "six" || flipped == "sev" || flipped == "eig" || flipped == "nin")
+                        {
+                            secondNum = flipped;
+                            break;
+                        }
+                    if (!string.IsNullOrEmpty(secondNum))
+                    {
+                        break;
+                    }
+                    check = "";
+                    flipped = "";
                 }
 
-                Console.WriteLine(sum);
-                Console.ReadLine();
+                check = "";
+                    switch(firstNum)
+                    {
+                        case "one":
+                            firstNum = "1";
+                            break;
+                        case "two":
+                            firstNum = "2";
+                            break;
+                        case "thr":
+                            firstNum = "3";
+                            break;
+                        case "fou":
+                            firstNum = "4";
+                            break;
+                        case "fiv":
+                            firstNum = "5";
+                            break;
+                        case "six":
+                            firstNum = "6";
+                            break;
+                        case "sev":
+                            firstNum = "7";
+                            break;
+                        case "eig":
+                            firstNum = "8";
+                            break;
+                        case "nin":
+                            firstNum = "9";
+                            break;
+                    }
+
+                    switch (secondNum)
+                    {
+                        case "one":
+                        secondNum = "1";
+                            break;
+                        case "two":
+                        secondNum = "2";
+                            break;
+                        case "thr":
+                        secondNum = "3";
+                            break;
+                        case "fou":
+                        secondNum = "4";
+                            break;
+                        case "fiv":
+                        secondNum = "5";
+                            break;
+                        case "six":
+                        secondNum = "6";
+                            break;
+                        case "sev":
+                        secondNum = "7";
+                            break;
+                        case "eig":
+                        secondNum = "8";
+                            break;
+                        case "nin":
+                        secondNum = "9";
+                            break;
+                    }
+                    sum = firstNum + secondNum;
+                    numbers.Add(sum);
+                    sum = "";
+                }
+
+            int totalNumber = 0;
+
+                foreach(string s in numbers)
+                {
+                    
+                   int number = Convert.ToInt32(s);
+                   Console.WriteLine(number);
+
+                   totalNumber += number;
+                }
+
+            Console.WriteLine("Total number is: " + totalNumber);
+            Console.ReadLine();
             
+
+        }
+
+        private static string Flip(string check)
+        {
+            char[] chArr = check.ToCharArray();
+            Array.Reverse(chArr);
+            return new string(chArr);
         }
     }
 }
