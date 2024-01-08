cf6193a437be7bda1d1dbf72542b3e798e457a8b
relavrin <rebeka.lavrin@gmail.com>
2024-01-08 14:52:50 +0100

    Start of Day 3

---
+++ Day3/Day3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Day3
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if(string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                lines.Add(input);
+            }
+
+            foreach (string s in lines)
+            {
+
+            }
+        }
+    }
+}
